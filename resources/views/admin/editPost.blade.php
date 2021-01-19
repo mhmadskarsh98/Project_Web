8954bb604ec403f5fae28fe80f2566e49509906a
Your Name <you@example.com>
2021-01-19 22:19:56 +0200

    post task

--- resources/views/admin/editPost.blade.php
+++ resources/views/admin/editPost.blade.php
@@ -1,92 +1,5 @@
-<html lang="en">
-  <head>
-    <meta charset="utf-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <title>Blog</title>
-    <!-- Styles -->
-    <!-- Latest compiled and minified CSS -->
-    <link
-      rel="stylesheet"
-      href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
-      integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u"
-      crossorigin="anonymous"
-    />
-  </head>
-  <body>
-    <div id="app">
-      <nav class="navbar navbar-default navbar-static-top">
-        <div class="container">
-          <div class="navbar-header">
-            <!-- Collapsed Hamburger -->
-            <button
-              type="button"
-              class="navbar-toggle collapsed"
-              data-toggle="collapse"
-              data-target="#app-navbar-collapse"
-            >
-              <span class="sr-only">Toggle Navigation</span>
-              <span class="icon-bar"></span>
-              <span class="icon-bar"></span>
-              <span class="icon-bar"></span>
-            </button>
-
-            <!-- Branding Image -->
-            <a class="navbar-brand" href="">
-              Blog
-            </a>
-          </div>
-
-          <div class="collapse navbar-collapse" id="app-navbar-collapse">
-            <!-- Left Side Of Navbar -->
-
-            <ul class="nav navbar-nav">
-              <li><a href="/admin/posts">Posts</a></li>
-              <li>
-                <a href="/admin/categories">Categories</a>
-              </li>
-              <li>
-                <a href="/admin/comments">Comments</a>
-              </li>
-              <li><a href="/admin/tags">Tags</a></li>
-
-              <li><a href="/admin/users">Users</a></li>
-            </ul>
-
-            <!-- Right Side Of Navbar -->
-            <ul class="nav navbar-nav navbar-right">
-              <!-- Authentication Links -->
-              <li class="dropdown">
-                <a
-                  href="#"
-                  class="dropdown-toggle"
-                  data-toggle="dropdown"
-                  role="button"
-                  aria-expanded="false"
-                >
-                  Admin <span class="caret"></span>
-                </a>
-
-                <ul class="dropdown-menu" role="menu">
-                  <li>
-                    <a href="/logout">
-                      Logout
-                    </a>
-
-                    <form
-                      id="logout-form"
-                      action="/logout"
-                      method="POST"
-                      style="display: none;"
-                    ></form>
-                  </li>
-                </ul>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
-
+@extends('layout')
+@section('content')
       <div class="container">
         <div class="row">
           <div class="col-md-12">
@@ -99,16 +12,20 @@
                     >Go Back</a
                   >
                 </h2>
+                @include('partials._errors')
               </div>
 
               <div class="panel-body">
                 <form
                   method="POST"
-                  action="/admin/posts/1"
+                  action="/admin/posts/{{$post->id}}"
                   accept-charset="UTF-8"
                   class="form-horizontal"
                   role="form"
                 >
+                {{-- <input type="hidden" name="_method" value="put"> --}}
+                @method('put')
+                @csrf
                   <div class="form-group">
                     <label for="title" class="col-md-2 control-label"
                       >Title</label
@@ -121,7 +38,7 @@
                         autofocus="autofocus"
                         name="title"
                         type="text"
-                        value="Quo ut eius magni ut adipisci perspiciatis non maxime."
+                        value="{{$post->title}}"
                         id="title"
                       />
 
@@ -130,6 +47,26 @@
                       </span>
                     </div>
                   </div>
+                  <div class="form-group">
+                    <label for="auther" class="col-md-2 control-label"
+                      >Auther</label
+                    >
+
+                  <div class="col-md-8 ">
+                      <input
+                        class="form-control"
+
+                        autofocus="autofocus"
+                        name="auther"
+                        type="text"
+                        id="auther"
+                        value="{{ $post->auther }}"
+                      />
+                      <span class="help-block">
+                        <strong></strong>
+                      </span>
+                    </div>
+                </div>
 
                   <div class="form-group">
                     <label for="body" class="col-md-2 control-label"
@@ -144,8 +81,7 @@
                         cols="50"
                         rows="10"
                         id="body"
-                      >
-Animi praesentium ad ipsum adipisci cum ea rerum quasi. Harum nam aliquid asperiores. Nesciunt natus rerum et ut ipsum. Voluptas numquam et cumque et. Enim mollitia harum fugit dignissimos id dignissimos tempore magnam. Suscipit sit est reprehenderit consequatur perspiciatis id. Est ullam officiis quia quibusdam sed magni ut dolor. Odit provident non consequuntur est. Autem iure placeat corporis consequatur aperiam ea. Et et officia velit sint aliquid. Harum labore aut recusandae quo. Laborum voluptatem eos aperiam esse illum sit magni. Consequatur ut praesentium velit praesentium aut aut ut tempora. Aperiam quo assumenda itaque iusto nihil sunt non. Occaecati id laudantium placeat nobis. Veritatis reiciendis quaerat natus qui in. Fugit voluptate quaerat quibusdam. Dignissimos perspiciatis dolores sint autem blanditiis saepe ut et. Perspiciatis facilis ut omnis tempore eum rem omnis. Molestiae dolorem sit magnam quo error dolores. Non sint perferendis laboriosam hic. Sed molestiae ipsum ullam facere porro. Dolor adipisci reiciendis et optio ullam quis qui ut. Voluptatem voluptate at quia nihil eligendi saepe. Ratione accusamus tempora non. Sunt qui commodi provident in quo. Qui nostrum aut saepe vel.</textarea
+                    >{{$post->body}}</textarea
                       >
 
                       <span class="help-block">
@@ -165,17 +101,40 @@ Animi praesentium ad ipsum adipisci cum ea rerum quasi. Harum nam aliquid asperi
                         required="required"
                         id="category_id"
                         name="category_id"
-                        ><option value="1">sedd</option
-                        ><option value="2">ut</option
-                        ><option value="3">id</option
-                        ><option value="4">occaecati</option
-                        ><option value="5">sint</option
-                        ><option value="6">quia</option
-                        ><option value="7" selected="selected">ipsum</option
-                        ><option value="8">quisquam</option
-                        ><option value="9">unde</option
-                        ><option value="10">voluptatum</option></select
-                      >
+
+                        >
+                          @foreach($categories as $cat )
+                          <option value={{$cat->id}}>
+                              {{$cat->name}}</option>
+                          @endforeach
+                      </select>
+
+                      <span class="help-block">
+                        <strong></strong>
+                      </span>
+                    </div>
+                  </div>
+
+                  <div class="form-group">
+                    <label for="tag_id" class="col-md-2 control-label"
+                      >Tag</label
+                    >
+
+                    <div class="col-md-8">
+                      <select
+                        class="form-control"
+                        required="required"
+                        id="tag_id"
+                        name="tag_id[]"
+                        multiple>
+
+                        @foreach ($tags as $tag)
+                        <option value="{{$tag->id}}"
+
+
+                            >{{$tag->name}}</option>
+                        @endforeach
+                        </select>
 
                       <span class="help-block">
                         <strong></strong>
@@ -196,6 +155,4 @@ Animi praesentium ad ipsum adipisci cum ea rerum quasi. Harum nam aliquid asperi
           </div>
         </div>
       </div>
-    </div>
-  </body>
-</html>
+@endsection
--- resources/views/admin/posts.blade.php
+++ resources/views/admin/posts.blade.php
@@ -1,76 +1,6 @@
-<html lang="en">
+@extends('layout')
 
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <title>Laravel Blog</title>
-
-    <!-- Styles -->
-    <!-- Latest compiled and minified CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u"
-        crossorigin="anonymous">
-
-</head>
-
-<body>
-
-    <nav class="navbar navbar-default navbar-static-top">
-        <div class="container">
-            <div class="navbar-header">
-
-                <!-- Collapsed Hamburger -->
-                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
-                    <span class="sr-only">Toggle Navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-
-                <!-- Branding Image -->
-                <a class="navbar-brand" href="">
-                    Laravel Blog
-                </a>
-            </div>
-
-            <div class="collapse navbar-collapse" id="app-navbar-collapse">
-                <!-- Left Side Of Navbar -->
-
-                <ul class="nav navbar-nav">
-                    <li><a href="admin/posts">Posts</a></li>
-                    <li><a href="admin/categories">Categories</a></li>
-                    <li><a href="admin/comments">Comments</a></li>
-                    <li><a href="admin/tags">Tags</a></li>
-
-                    <li><a href="admin/users">Users</a></li>
-                </ul>
-
-                <!-- Right Side Of Navbar -->
-                <ul class="nav navbar-nav navbar-right">
-                    <!-- Authentication Links -->
-                    <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                            Admin <span class="caret"></span>
-                        </a>
-
-                        <ul class="dropdown-menu" role="menu">
-                            <li>
-                                <a href="logout" onclick="event.preventDefault();
-                                                 document.getElementById('logout-form').submit();">
-                                    Logout
-                                </a>
-
-                                <form id="logout-form" action="logout" method="POST" style="display: none;">
-                                    <input type="hidden" name="_token" value="32Mxrb2s2QPyv3C1h4iYcbfZBT7PmU7Tfm9koxkk">
-                                </form>
-                            </li>
-                        </ul>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </nav>
+@section('content')
 
     <div class="container">
         <div class="row">
@@ -81,7 +11,7 @@
                         <h2>
                             Posts
 
-                            <a href=/posts/create" class="btn btn-default pull-right">Create New</a>
+                            <a href="/admin/posts/create" class="btn btn-default pull-right">Create New</a>
                         </h2>
                     </div>
 
@@ -99,181 +29,42 @@
                                 </tr>
                             </thead>
                             <tbody>
-                                <tr>
-                                    <td>Quo ut eius magni ut adipisci perspiciatis non maxime.</td>
-                                    <td>Animi praesentium ad ipsum adipisci cum ea rerum quasi. Haru...</td>
-                                    <td>Prof. General Huel IV</td>
-                                    <td>ipsum</td>
-                                    <td></td>
+
+                            @foreach ($posts as $item)
+                            <tr>
+                                    <td>{{$item->title}}</td>
+                                    <td>{{$item->body}}</td>
+                                    <td>{{ $item->auther }}</td>
+                                    <td>{{ $item->category->name }}</td>
+                                    <td>
+                                        @foreach ($item->tags as $tag)
+                                            {{$tag->name}}
+                                            <br>
+                                        @endforeach
+                                    </td>
                                     <td>No</td>
                                     <td>
-                                        <a href=/posts/1/publish" data-method="PUT" data-token="32Mxrb2s2QPyv3C1h4iYcbfZBT7PmU7Tfm9koxkk"
+                                        <a href="/posts/1/publish" data-method="PUT" data-token="32Mxrb2s2QPyv3C1h4iYcbfZBT7PmU7Tfm9koxkk"
                                             data-confirm="Are you sure?" class="btn btn-xs btn-warning">Publish</a>
-                                        <a href=/posts/1" class="btn btn-xs btn-success">Show</a>
-                                        <a href=/posts/1/edit" class="btn btn-xs btn-info">Edit</a>
-                                        <a href=/posts/1" data-method="DELETE" data-token="32Mxrb2s2QPyv3C1h4iYcbfZBT7PmU7Tfm9koxkk"
-                                            data-confirm="Are you sure?" class="btn btn-xs btn-danger">Delete</a>
+                                        <a href="/admin/posts/{{$item->id}}" class="btn btn-xs btn-success">Show</a>
+                                        <a href="/admin/posts/{{$item->id}}/edit" class="btn btn-xs btn-info">Edit</a>
+
+                                            <form style="display: inline;" action="/admin/posts/{{$item->id}}" method="post">
+                                                    @method('delete')
+                                                    @csrf
+                                                    <button type="submit" class="btn btn-xs btn-danger">Delete</button>
+                                            </form>
                                     </td>
                                 </tr>
-                                <tr>
-                                    <td>Similique ea eum odit aspernatur quibusdam atque.</td>
-                                    <td>Quae ratione error dicta pariatur sit molestiae. Eos quo lib...</td>
-                                    <td>Bella Kuhn V</td>
-                                    <td>unde</td>
-                                    <td>nam, sit, omnis</td>
-                                    <td>Yes</td>
-                                    <td>
-                                        <a href="/posts/2/publish" data-method="PUT" data-token="32Mxrb2s2QPyv3C1h4iYcbfZBT7PmU7Tfm9koxkk"
-                                            data-confirm="Are you sure?" class="btn btn-xs btn-warning">Draft</a>
-                                        <a href="/posts/2" class="btn btn-xs btn-success">Show</a>
-                                        <a href="/posts/2/edit" class="btn btn-xs btn-info">Edit</a>
-                                        <a href="/posts/2" data-method="DELETE" data-token="32Mxrb2s2QPyv3C1h4iYcbfZBT7PmU7Tfm9koxkk"
-                                            data-confirm="Are you sure?" class="btn btn-xs btn-danger">Delete</a>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td>Culpa odio hic est quod dolorum rerum.</td>
-                                    <td>Perspiciatis nesciunt nesciunt et maiores porro sunt. Provid...</td>
-                                    <td>Stone Hirthe</td>
-                                    <td>id</td>
-                                    <td>non, et</td>
-                                    <td>No</td>
-                                    <td>
-                                        <a href="/posts/3/publish" data-method="PUT" data-token="32Mxrb2s2QPyv3C1h4iYcbfZBT7PmU7Tfm9koxkk"
-                                            data-confirm="Are you sure?" class="btn btn-xs btn-warning">Publish</a>
-                                        <a href="/posts/3" class="btn btn-xs btn-success">Show</a>
-                                        <a href="/posts/3/edit" class="btn btn-xs btn-info">Edit</a>
-                                        <a href="/posts/3" data-method="DELETE" data-token="32Mxrb2s2QPyv3C1h4iYcbfZBT7PmU7Tfm9koxkk"
-                                            data-confirm="Are you sure?" class="btn btn-xs btn-danger">Delete</a>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td>Quia harum tempora vitae illo sed.</td>
-                                    <td>Ullam qui dicta odit sint rerum in enim odit. Esse debitis m...</td>
-                                    <td>Lacey Gaylord DVM</td>
-                                    <td>unde</td>
-                                    <td>et, nam, molestias</td>
-                                    <td>Yes</td>
-                                    <td>
-                                        <a href="/posts/4/publish" data-method="PUT" data-token="32Mxrb2s2QPyv3C1h4iYcbfZBT7PmU7Tfm9koxkk"
-                                            data-confirm="Are you sure?" class="btn btn-xs btn-warning">Draft</a>
-                                        <a href="/posts/4" class="btn btn-xs btn-success">Show</a>
-                                        <a href="/posts/4/edit" class="btn btn-xs btn-info">Edit</a>
-                                        <a href="/posts/4" data-method="DELETE" data-token="32Mxrb2s2QPyv3C1h4iYcbfZBT7PmU7Tfm9koxkk"
-                                            data-confirm="Are you sure?" class="btn btn-xs btn-danger">Delete</a>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td>Modi hic dolor qui sit.</td>
-                                    <td>Consequatur perspiciatis deleniti quia laborum omnis sed arc...</td>
-                                    <td>Mrs. Izabella Bashirian Sr.</td>
-                                    <td>ut</td>
-                                    <td></td>
-                                    <td>No</td>
-                                    <td>
-                                        <a href="/posts/5/publish" data-method="PUT" data-token="32Mxrb2s2QPyv3C1h4iYcbfZBT7PmU7Tfm9koxkk"
-                                            data-confirm="Are you sure?" class="btn btn-xs btn-warning">Publish</a>
-                                        <a href="/posts/5" class="btn btn-xs btn-success">Show</a>
-                                        <a href="/posts/5/edit" class="btn btn-xs btn-info">Edit</a>
-                                        <a href="/posts/5" data-method="DELETE" data-token="32Mxrb2s2QPyv3C1h4iYcbfZBT7PmU7Tfm9koxkk"
-                                            data-confirm="Are you sure?" class="btn btn-xs btn-danger">Delete</a>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td>Est ea ea modi est est consequatur culpa.</td>
-                                    <td>Vel iusto et est sit voluptas et veniam. Illum aut ipsa aliq...</td>
-                                    <td>Favian Halvorson</td>
-                                    <td>voluptatum</td>
-                                    <td>illum, ad</td>
-                                    <td>Yes</td>
-                                    <td>
-                                        <a href=/posts/6/publish" data-method="PUT" data-token="32Mxrb2s2QPyv3C1h4iYcbfZBT7PmU7Tfm9koxkk"
-                                            data-confirm="Are you sure?" class="btn btn-xs btn-warning">Draft</a>
-                                        <a href=/posts/6" class="btn btn-xs btn-success">Show</a>
-                                        <a href=/posts/6/edit" class="btn btn-xs btn-info">Edit</a>
-                                        <a href=/posts/6" data-method="DELETE" data-token="32Mxrb2s2QPyv3C1h4iYcbfZBT7PmU7Tfm9koxkk"
-                                            data-confirm="Are you sure?" class="btn btn-xs btn-danger">Delete</a>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td>Est dolorum vel repellat non mollitia et laudantium.</td>
-                                    <td>Officia consequatur ut nihil. Dolorum perspiciatis quibusdam...</td>
-                                    <td>Admin</td>
-                                    <td>unde</td>
-                                    <td>non, et</td>
-                                    <td>No</td>
-                                    <td>
-                                        <a href=/posts/7/publish" data-method="PUT" data-token="32Mxrb2s2QPyv3C1h4iYcbfZBT7PmU7Tfm9koxkk"
-                                            data-confirm="Are you sure?" class="btn btn-xs btn-warning">Publish</a>
-                                        <a href=/posts/7" class="btn btn-xs btn-success">Show</a>
-                                        <a href=/posts/7/edit" class="btn btn-xs btn-info">Edit</a>
-                                        <a href=/posts/7" data-method="DELETE" data-token="32Mxrb2s2QPyv3C1h4iYcbfZBT7PmU7Tfm9koxkk"
-                                            data-confirm="Are you sure?" class="btn btn-xs btn-danger">Delete</a>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td>Deleniti quisquam ipsam sint veritatis neque culpa quae.</td>
-                                    <td>Autem alias sed eos est doloremque quod neque. Et sapiente v...</td>
-                                    <td>Bella Kuhn V</td>
-                                    <td>quisquam</td>
-                                    <td>illum, nam, illum</td>
-                                    <td>No</td>
-                                    <td>
-                                        <a href=/posts/8/publish" data-method="PUT" data-token="32Mxrb2s2QPyv3C1h4iYcbfZBT7PmU7Tfm9koxkk"
-                                            data-confirm="Are you sure?" class="btn btn-xs btn-warning">Publish</a>
-                                        <a href=/posts/8" class="btn btn-xs btn-success">Show</a>
-                                        <a href=/posts/8/edit" class="btn btn-xs btn-info">Edit</a>
-                                        <a href=/posts/8" data-method="DELETE" data-token="32Mxrb2s2QPyv3C1h4iYcbfZBT7PmU7Tfm9koxkk"
-                                            data-confirm="Are you sure?" class="btn btn-xs btn-danger">Delete</a>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td>Sapiente dolorum quo non voluptates atque dolore.</td>
-                                    <td>Nam cum sint totam id quibusdam molestiae. Corporis cupidita...</td>
-                                    <td>Stone Hirthe</td>
-                                    <td>ut</td>
-                                    <td>debitis, nihil, sit, non</td>
-                                    <td>Yes</td>
-                                    <td>
-                                        <a href=/posts/9/publish" data-method="PUT" data-token="32Mxrb2s2QPyv3C1h4iYcbfZBT7PmU7Tfm9koxkk"
-                                            data-confirm="Are you sure?" class="btn btn-xs btn-warning">Draft</a>
-                                        <a href=/posts/9" class="btn btn-xs btn-success">Show</a>
-                                        <a href=/posts/9/edit" class="btn btn-xs btn-info">Edit</a>
-                                        <a href=/posts/9" data-method="DELETE" data-token="32Mxrb2s2QPyv3C1h4iYcbfZBT7PmU7Tfm9koxkk"
-                                            data-confirm="Are you sure?" class="btn btn-xs btn-danger">Delete</a>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td>Vitae non et et nisi amet.</td>
-                                    <td>At eveniet aperiam eveniet placeat libero expedita autem. El...</td>
-                                    <td>Favian Halvorson</td>
-                                    <td>sint</td>
-                                    <td>non, ad, molestias</td>
-                                    <td>Yes</td>
-                                    <td>
-                                        <a href=/posts/10/publish" data-method="PUT" data-token="32Mxrb2s2QPyv3C1h4iYcbfZBT7PmU7Tfm9koxkk"
-                                            data-confirm="Are you sure?" class="btn btn-xs btn-warning">Draft</a>
-                                        <a href=/posts/10" class="btn btn-xs btn-success">Show</a>
-                                        <a href=/posts/10/edit" class="btn btn-xs btn-info">Edit</a>
-                                        <a href=/posts/10" data-method="DELETE" data-token="32Mxrb2s2QPyv3C1h4iYcbfZBT7PmU7Tfm9koxkk"
-                                            data-confirm="Are you sure?" class="btn btn-xs btn-danger">Delete</a>
-                                    </td>
-                                </tr>
+                            @endforeach
                             </tbody>
                         </table>
 
-                        <ul class="pagination">
 
-                            <li class="disabled"><span>«</span></li>
+                        <div class="text-center">
+                            {{$posts->render()}}
+                        </div>
 
-
-
-
-
-                            <li class="active"><span>1</span></li>
-                            <li><a href=/posts?page=2">2 </a> </li> <li><a href=/posts?page=3">3 </a> </li> <li><a href=/posts?page=2"
-                                            rel="next">»</a></li>
-                        </ul>
 
 
                     </div>
@@ -282,6 +73,4 @@
 
         </div>
     </div>
-</body>
-
-</html>
+    @endsection
